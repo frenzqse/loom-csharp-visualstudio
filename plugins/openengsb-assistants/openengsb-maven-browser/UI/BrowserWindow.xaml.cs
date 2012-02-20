c67a2c8619ac7aece785dc071a6eb99b1e91b50f
Xiaolin Zhang <xiaolin.zhang@gmx.net>
2012-02-20 17:00:14 +0100

    project skeleton created

---
+++ plugins/openengsb-assistants/openengsb-maven-browser/UI/BrowserWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Org.OpenEngSB.Loom.Csharp.VisualStudio.Plugins.Assistants.UI
+{
+    /// <summary>
+    /// Interaction logic for BrowserWindow.xaml
+    /// </summary>
+    public partial class BrowserWindow : Window
+    {
+        public BrowserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Visibility = System.Windows.Visibility.Hidden;
+        }
+    }
+}
